25b47c0dd3309f599fe1de3bcb1748fc5e88664d
Brian Clark <bclarkx2@gmail.com>
2017-12-17 14:46:30 -0500

    Refactored discriminators

--- babble/GAN.py
+++ babble/GAN.py
@@ -8,13 +8,15 @@
 import argparse
 
 import numpy as np
-from keras.layers import Conv1D, Dropout, Flatten, Dense, Activation, BatchNormalization, Reshape, \
+from keras.layers import Dropout, Flatten, Dense, Activation, BatchNormalization, Reshape, \
     UpSampling2D, Conv2DTranspose
 from keras.models import Sequential
 from keras.optimizers import RMSprop
 from keras.preprocessing.text import Tokenizer
 
 # LOCAL
+from discriminator import basic_discriminator
+
 
 np.random.seed(1234)
 
@@ -38,7 +40,7 @@ class GAN(object):
     SENTENCE_LENGTH = 3
     NOISE_SIZE = 100
 
-    def __init__(self):
+    def __init__(self, disc):
 
         self.tkn = tokenizer(SIMPLE_WORDS)
         self.corpus = corpus(SIMPLE_SENTENCES)
@@ -48,48 +50,11 @@ class GAN(object):
         self.pos_sentences = sequences(self.tkn, SIMPLE_SENTENCES)
         self.neg_sentences = sequences(self.tkn, SIMPLE_NEGATIVES)
 
-        self.disc = self._disc()
+        self.disc = disc
         self.gen = self._gen()
 
         self.disc_flow = self._disc_flow()
         self.adv_flow = self._adv_flow()
-
-    def _disc(self):
-
-        self.disc = Sequential()
-
-        depth = 64
-        dropout = 0.4
-
-        # In: 28 x 28 x 1, depth = 1
-        # Out: 14 x 14 x 1, depth=64
-
-        # input_shape = (GAN.SENTENCE_LENGTH, 1)
-        input_shape = self.pos_sentences.shape[1:]
-        # padding='same'
-        # strides=1
-        self.disc.add(Conv1D(2, 2, input_shape=input_shape))
-        # self.disc.add(LeakyReLU(alpha=0.2))
-        # self.disc.add(Dropout(dropout))
-
-        # self.disc.add(Conv2D(depth*2, 5, strides=2, padding='same'))
-        # self.disc.add(LeakyReLU(alpha=0.2))
-        # self.disc.add(Dropout(dropout))
-        #
-        # self.disc.add(Conv2D(depth*4, 5, strides=2, padding='same'))
-        # self.disc.add(LeakyReLU(alpha=0.2))
-        # self.disc.add(Dropout(dropout))
-
-        # self.disc.add(Conv2D(depth*8, 5, strides=1, padding='same'))
-        # self.disc.add(LeakyReLU(alpha=0.2))
-        # self.disc.add(Dropout(dropout))
-
-        # Out: 1-dim probability
-        self.disc.add(Flatten())
-        self.disc.add(Dense(1))
-        self.disc.add(Activation('sigmoid'))
-        self.disc.summary()
-        return self.disc
 
     def _disc_flow(self):
         optimizer = RMSprop(lr=0.0002, decay=6e-8)
@@ -269,14 +234,15 @@ def main():
 
     args = get_args()
 
-    gan = GAN()
+    disc = basic_discriminator(shape=(3,1))
+    gan = GAN(disc)
 
     # print(gan.pos_sentences)
     # print(gan.neg_sentences)
 
     gan.train_disc(train_itr=2000)
 
-    gan.train_adv(train_itr=10000)
+    # gan.train_adv(train_itr=10000)
 
 
 if __name__ == '__main__':
---
+++ babble/discriminator.py
@@ -0,0 +1,50 @@
+
+###############################################################################
+# Imports                                                                     #
+###############################################################################
+
+from keras import Sequential
+from keras.layers import Conv1D, Flatten, Dense, Activation
+
+
+###############################################################################
+# Discriminators                                                              #
+###############################################################################
+
+def basic_discriminator(shape=(3,1)):
+
+    disc = Sequential()
+
+    depth = 64
+    dropout = 0.4
+
+    # In: 28 x 28 x 1, depth = 1
+    # Out: 14 x 14 x 1, depth=64
+
+    # input_shape = (GAN.SENTENCE_LENGTH, 1)
+    # input_shape = self.pos_sentences.shape[1:]
+    # padding='same'
+    # strides=1
+    disc.add(Conv1D(2, 2, input_shape=shape))
+    # disc.add(LeakyReLU(alpha=0.2))
+    # disc.add(Dropout(dropout))
+
+    # disc.add(Conv2D(depth*2, 5, strides=2, padding='same'))
+    # disc.add(LeakyReLU(alpha=0.2))
+    # disc.add(Dropout(dropout))
+    #
+    # disc.add(Conv2D(depth*4, 5, strides=2, padding='same'))
+    # disc.add(LeakyReLU(alpha=0.2))
+    # disc.add(Dropout(dropout))
+
+    # disc.add(Conv2D(depth*8, 5, strides=1, padding='same'))
+    # disc.add(LeakyReLU(alpha=0.2))
+    # disc.add(Dropout(dropout))
+
+    # Out: 1-dim probability
+    disc.add(Flatten())
+    disc.add(Dense(1))
+    disc.add(Activation('sigmoid'))
+    disc.summary()
+    return disc
+
